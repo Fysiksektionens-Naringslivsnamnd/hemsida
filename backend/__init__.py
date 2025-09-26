3b6f64e3b75507b758059eb8ffd99bfd17fdaa05
Erik Oskar Duvander <duvander@kth.se>
2025-09-26 20:19:29 +0200

    noice

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+__version__ = "1.0.0"
+
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 load_dotenv()
